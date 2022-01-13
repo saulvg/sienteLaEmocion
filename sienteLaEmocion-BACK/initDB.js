71e6f4464bc597b59bed3b28627f67a8fa8eef72
saul vaquero <saulvgproyecto@gmail.com>
2022-01-13 17:13:57 +0100

    actualizacion de la base de datos

--- sienteLaEmocion-BACK/initDB.js
+++ sienteLaEmocion-BACK/initDB.js
@@ -19,6 +19,7 @@ async function initDB() {
 
   try {
     connection = await getDB();
+    await connection.query('DROP TABLE IF EXISTS votes');
     await connection.query('DROP TABLE IF EXISTS users');
     await connection.query('DROP TABLE IF EXISTS experiences');
     await connection.query('DROP TABLE IF EXISTS address');
@@ -27,7 +28,6 @@ async function initDB() {
     await connection.query('DROP TABLE IF EXISTS experiences_description');
     await connection.query('DROP TABLE IF EXISTS experiences_category');
     await connection.query('DROP TABLE IF EXISTS company');
-    await connection.query('DROP TABLE IF EXISTS votes');
     await connection.query('DROP TABLE IF EXISTS booking');
 
     await connection.query(`
@@ -52,7 +52,7 @@ async function initDB() {
         )
     `);
     await connection.query(`
-        CREATE TABLE experiences   (
+        CREATE TABLE experiences (
             id INT PRIMARY KEY AUTO_INCREMENT,
             id_user INT NOT NULL,
             id_company INT NOT NULL,
@@ -64,69 +64,64 @@ async function initDB() {
             name VARCHAR (50) NOT NULL,
             createdAt DATETIME NOT NULL, 
             modifiedAt DATETIME, 
+            FOREIGN KEY (id_user) REFERENCES users(id),
+            FOREIGN KEY (id_company) REFERENCES company(id),
+            FOREIGN KEY (id_address) REFERENCES address(id),
+            FOREIGN KEY (id_experiences_category) REFERENCES experiences_category(id)
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE address (
+            id INT PRIMARY KEY AUTO_INCREMENT
+
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE experiences_photos (
+            id INT PRIMARY KEY AUTO_INCREMENT
+
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE experiences_description_type (
+            id INT PRIMARY KEY AUTO_INCREMENT
+
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE experiences_description (
+            id INT PRIMARY KEY AUTO_INCREMENT
+
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE experiences_category (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(50) NOT NULL
+                
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE company (
+            id INT PRIMARY KEY AUTO_INCREMENT
+
+        )
+    `);
+    await connection.query(`
+        CREATE TABLE votes (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            experiences_id INT NOT NULL,
+            vote TINYINT,
+            review VARCHAR(255),
+            createdAt DATETIME NOT NULL,
+            id_user INT NOT NULL,
             FOREIGN KEY(id_user) REFERENCES users(id),
-            FOREIGN KEY(id_company) REFERENCES company(id),
-            FOREIGN KEY(id_address) REFERENCES address(id),
-            FOREIGN KEY(id_experiences_category) REFERENCES experiences_category (id)
-
-
-
-
-
-        )
-    `);
-    await connection.query(`
-        CREATE TABLE address   (
-            id INT PRIMARY KEY AUTO_INCREMENT
-
-        )
-    `);
-    await connection.query(`
-        CREATE TABLE experiences_photos   (
-            id INT PRIMARY KEY AUTO_INCREMENT
-
-        )
-    `);
-    await connection.query(`
-        CREATE TABLE experiences_description_type  (
-            id INT PRIMARY KEY AUTO_INCREMENT
-
-        )
-    `);
-    await connection.query(`
-        CREATE TABLE experiences_description   (
-            id INT PRIMARY KEY AUTO_INCREMENT
-
-        )
-    `);
-    await connection.query(`
-    CREATE TABLE experiences_category   (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(50) NOT NULL
+            FOREIGN KEY (experiences_id) REFERENCES experiences(id) ON DELETE CASCADE 
             
-    )
-    `);
-    await connection.query(`
-        CREATE TABLE company   (
-            id INT PRIMARY KEY AUTO_INCREMENT
-
         )
     `);
     await connection.query(`
-            CREATE TABLE votes   (
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                experiences_id INT NOT NULL,
-                vote TINYINT,
-                review VARCHAR(255),
-                createdAt DATETIME NOT NULL,
-                id_user INT NOT NULL,
-                FOREIGN KEY(id_user) REFERENCES users(id),
-                FOREIGN KEY (experiences_id) REFERENCES experiences(id) ON DELETE CASCADE 
-            )
-        )
-    `);
-    await connection.query(`
-        CREATE TABLE booking   (
+        CREATE TABLE booking (
             id INT PRIMARY KEY AUTO_INCREMENT
 
         )
