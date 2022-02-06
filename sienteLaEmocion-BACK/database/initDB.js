2c84777f2de9a33f9ea787c207bebfc0882b657d
javierlg91 <javilogut91@gmail.com>
2022-02-06 18:39:57 +0000

    Update initDB.js

--- sienteLaEmocion-BACK/database/initDB.js
+++ sienteLaEmocion-BACK/database/initDB.js
@@ -16,12 +16,12 @@ async function initDB() {
         connection = await getDB();
         await connection.query('DROP TABLE IF EXISTS votes');
         await connection.query('DROP TABLE IF EXISTS booking');
-        await connection.query('DROP TABLE IF EXISTS experiences_photos');
-        await connection.query('DROP TABLE IF EXISTS experiences'); //Añadir los campos de las tablas "experiences_description", "experiences_description_type" y "address"
-        await connection.query('DROP TABLE IF EXISTS company');
-        await connection.query('DROP TABLE IF EXISTS experiences_category');
-        await connection.query('DROP TABLE IF EXISTS users');
         await connection.query('DROP TABLE IF EXISTS my_experiences');
+        await connection.query('DROP TABLE IF EXISTS experiences_photos');
+        await connection.query('DROP TABLE IF EXISTS experiences_category');
+        await connection.query('DROP TABLE IF EXISTS company');
+        await connection.query('DROP TABLE IF EXISTS experiences'); //Añadir los campos de las tablas "experiences_description", "experiences_description_type" y "address"
+        await connection.query('DROP TABLE IF EXISTS users');
 
         await connection.query(`
         CREATE TABLE users (
