fff3d310472ff5ec0958d11242b1e2674650979c
saulvg <saulvgproyecto@gmail.com>
2022-02-23 10:34:36 +0000

    headerhomepage listaActividades a medias

--- siente-la-emocion-front/src/components/Footer/Footer.js
+++ siente-la-emocion-front/src/components/Footer/Footer.js
@@ -1,21 +1,25 @@
+import './footer.css'
+
 const Footer = () => {
 return(
     <footer>
         <h2>Experiencia diferente</h2>
-        <section id='visitanos'>
-          <h3>Visitanos</h3>
-          <p>calle XXXXXX pascual manual</p>
-        </section>
-        <section id='llamanos'>
-          <h3>Llamanos</h3>
-          <p>123456789</p>
-        </section>
-        <section id='llamanos'>
-          <h3>Cookies</h3>
-          <p>
-            <a href='cookies'>Enlace de cookies</a>
-          </p>
-        </section>
+        <div className='footerMain'>
+          <section id='visitanos'>
+            <h3>Visitanos</h3>
+            <p>calle XXXXXX pascual manual</p>
+          </section>
+          <section id='llamanos'>
+            <h3>Llamanos</h3>
+            <p>123456789</p>
+          </section>
+          <section id='llamanos'>
+            <h3>Cookies</h3>
+            <p>
+              <a href='cookies'>Enlace de cookies</a>
+            </p>
+          </section>
+        </div>
         <p>todos los derechos estan reservados a bla bla</p>
     </footer>
 )
--- siente-la-emocion-front/src/pages/homePage/HomePage.js
+++ siente-la-emocion-front/src/pages/homePage/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = () =>{
       <div className="App">
         <Header to={'/listaActividades'} button={'Atrevete'}/>
         <main>
-          <section id='we'>
+          <section id='we' className='interseccion'>
             <CircleHomePage id={'routeDeQueVaEsto'} clas={'homePage'} href={'#deQueVaEsto'} children={'De que trata esto'}/>
             <CircleHomePage id={'routeComents'} clas={'homePage'} href={'#coments'} children={'Comentarios'}/>
             <CircleHomePage id={'routeNuestrosPrincipios'} clas={'homePage'} href={'#nuestrosPrincipios'} children={'Nuestros principios'}/>
@@ -28,7 +28,7 @@ const HomePage = () =>{
               p2={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.'}
             />
           </section>
-          <section id='socialNetworks'>
+          <section id='socialNetworks' className='interseccion'>
             <SocialNetwork id={'instagram'} href={'https://www.instagram.com/'} children={'instagram'}/>
             <SocialNetwork id={'gmail'} href={'https://www.google.com/intl/es/gmail/about/'} children={'gmail'}/>
             <SocialNetwork id={'facebook'} href={'https:/es-es.facebook.com/'} children={'facebook'}/>
@@ -40,7 +40,7 @@ const HomePage = () =>{
               p2={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.'}
             />
           </section>
-          <section id='coments'>
+          <section id='coments' className='interseccion'>
             <CircleHomePage id={'comment1'} children={'Comentario destacado 1'}/>
             <CircleHomePage id={'comment2'} children={'Comentario destacado 2'}/>
             <CircleHomePage id={'comment3'} children={'Comentario destacado 3'}/>
@@ -52,6 +52,7 @@ const HomePage = () =>{
               p2={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.'}
             />
           </section>
+          <section className='interseccionBlanco'/>
   
         </main>
       </div>
